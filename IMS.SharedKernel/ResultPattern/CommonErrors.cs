161e623fec2f13002d4bd30b6264b215d8a0d37b
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-09-07 01:59:14 +0300

    Add common errors

---
+++ IMS.SharedKernel/ResultPattern/CommonErrors.cs
@@ -0,0 +1,14 @@
+﻿namespace IMS.SharedKernel.ResultPattern
+{
+    public static class CommonErrors
+    {
+        public static Error InvalidFieldError(string entityName, string fieldName)
+        {
+            return new Error(
+                $"{entityName}.{fieldName}.Invalid",
+                $"{entityName} {fieldName} is invalid.",
+                ErrorType.Validation
+            );
+        }
+    }
+}
