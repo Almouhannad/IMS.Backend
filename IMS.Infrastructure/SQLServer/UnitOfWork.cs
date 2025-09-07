6e0880f957bd277b10374dd59ccd4f86aa0df651
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-09-07 20:43:34 +0300

    Implement UOW

---
+++ IMS.Infrastructure/SQLServer/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using IMS.Domain.Interfaces;
+using IMS.SharedKernel.ResultPattern;
+
+namespace IMS.Infrastructure.SQLServer;
+
+public sealed class UnitOfWork(IMSDBContext context) : IUnitOfWork
+{
+    private readonly IMSDBContext _context = context;
+    public IProductRepository Products { get; } = new ProductRepository(context);
+    public ICategoryRepository Categories { get; } = new CategoryRepository(context);
+
+    public async Task<Result> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+        catch (Exception)
+        {
+            return Result.Failure(new Error("Persistence.Failure", "Unable to persist data", ErrorType.Failure));
+        }
+        
+        return Result.Success();
+    }
+}
