d19215c46841c87a9e4ef25e9d4740ec1b7586b4
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-09-08 00:28:55 +0300

    Refactor infrastructure tests to AAA and add unit tests

--- IMS.Tests/Integration/Infrastructure/SQLServer/CategoryRepositoryTests.cs
+++ IMS.Tests/Integration/Infrastructure/SQLServer/CategoryRepositoryTests.cs
@@ -10,21 +10,23 @@ public class CategoryRepositoryTests
     [Fact]
     public async Task CreateAndGetByNameAsync_NormalizesName()
     {
+        // Arrange
         var options = new DbContextOptionsBuilder<IMSDBContext>()
             .UseInMemoryDatabase(Guid.NewGuid().ToString())
             .Options;
 
         await using var context = new IMSDBContext(options);
         await context.Database.EnsureCreatedAsync();
-
         var repository = new CategoryRepository(context, NullLogger<CategoryRepository>.Instance);
-
         var category = Category.Create(Guid.NewGuid(), "  Electronics ").Value;
-        var createResult = await repository.CreateAsync(category);
-        Assert.True(createResult.IsSuccess);
-        await context.SaveChangesAsync();
 
+        // Act
+        var createResult = await repository.CreateAsync(category);
+        await context.SaveChangesAsync();
         var getResult = await repository.GetByNameAsync("ELECTRONICS");
+
+        // Assert
+        Assert.True(createResult.IsSuccess);
         Assert.True(getResult.IsSuccess);
         Assert.NotNull(getResult.Value);
         Assert.Equal(category.Id, getResult.Value!.Id);
