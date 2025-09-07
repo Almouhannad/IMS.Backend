955d7cb0baff5847a03a330cff3a77a045566760
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-09-07 22:35:45 +0300

    Configure infra DI

---
+++ IMS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using IMS.Config;
+using IMS.Domain.Interfaces;
+using IMS.Infrastructure.SQLServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IMS.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterSQLServerPersistenceFromInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<IMSDBContext>(options =>
+        {
+            options.UseSqlServer(CONFIG.SqlServerConnectionString);
+        });
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+
+    public static void ApplySQLServerMigrationsFromInfrastructure(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        using IMSDBContext context = scope.ServiceProvider.GetRequiredService<IMSDBContext>();
+
+        context.Database.Migrate();
+    }
+
+}
