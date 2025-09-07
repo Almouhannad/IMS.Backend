0fff1c41afc07e55bd9d64834cbf21b8d9ac7bfa
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-09-07 16:01:17 +0300

    Add config project

---
+++ IMS.Config/Config.cs
@@ -0,0 +1,14 @@
+﻿using IMS.SharedKernel.Config;
+
+namespace IMS.Config;
+
+public static class CONFIG
+{
+    // Env-var names
+    private const string SqlServerConnectionStringVar = "SQL_SERVER_CONNECTION_STRING";
+
+    // Lazy, exception-safe properties
+
+    private static readonly Lazy<string> _sqlServerConnectionString = new(() => ENVHelper.GetEnv(SqlServerConnectionStringVar));
+    public static string SqlServerConnectionString => _sqlServerConnectionString.Value;
+}
