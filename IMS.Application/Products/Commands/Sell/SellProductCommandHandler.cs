31d0ab6c8a28adfe321c3a1808189009c1054919
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-09-08 02:41:45 +0300

    Give handler via DI to ensure pipeline

--- IMS.Application/Products/Commands/Sell/SellProductCommandHandler.cs
+++ IMS.Application/Products/Commands/Sell/SellProductCommandHandler.cs
@@ -5,10 +5,12 @@ using IMS.SharedKernel.ResultPattern;
 
 namespace IMS.Application.Products.Commands.Sell;
 
-public sealed class SellProductCommandHandler(IUnitOfWork unitOfWork) : ICommandHandler<SellProductCommand>
+public sealed class SellProductCommandHandler(
+    ICommandHandler<ChangeProductStatusCommand> changeProductStatusCommandHandler)
+    : ICommandHandler<SellProductCommand>
 {
-    private readonly ChangeProductStatusCommandHandler _changeProductStatusCommandHandler = new(unitOfWork);
-
+    private readonly ICommandHandler<ChangeProductStatusCommand> _changeProductStatusCommandHandler =
+        changeProductStatusCommandHandler;
     public async Task<Result> Handle(SellProductCommand command, CancellationToken cancellationToken)
     {
         var changeStatusCommand = new ChangeProductStatusCommand(command.Id, "Sold");
